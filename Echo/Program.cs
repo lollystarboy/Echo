3ab34f23038bb9fe98618b6a0d7aa85d4d445897
cherneckiy <deneff.max@yandex.ru>
2024-08-26 13:07:54 +0300

    Добавьте файлы проекта.

---
+++ Echo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Echo
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Напишите что-то");
+                var str = Console.ReadLine();
+
+                Console.WriteLine("Укажите глубину эха");
+                var deep = int.Parse(Console.ReadLine());
+
+                Echo(str, deep);
+
+                Console.ReadKey();
+            }
+            static void Echo(string saidworld, int deep)
+            {
+                var modif = saidworld;
+                Console.WriteLine("..."+ modif);
+
+                if (modif.Length > 2)
+                {
+                    modif = modif.Remove(0, 2);
+                }
+                if (deep > 1)
+                { Echo(modif, deep - 1); }
+            }
+        
+    }
+}
